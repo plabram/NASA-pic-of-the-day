a5e41efc1b81b0d227d23dca02a459d7aa2a3f24
Penelope <freyalabram@gmail.com>
2023-07-16 13:20:54 +0200

    use Axios for API call

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import Card from './components/Card';
 import { useForm } from 'react-hook-form';
 import Form from './components/Form';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import axios from 'axios';
 
 function App() {
   const today = new Date(Date.now()).toISOString().slice(0, 10);
   const [date, setDate] = useState(today)
   const mainTitle = "Imagen astronómica del día"
   const [APOD, setAPOD] = useState({})
-  const [mission, setMission] = useState("all")
+  const URL = `${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`
+  const roverURL = `${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`
+
+  const [mission, setMission] = useState(URL)
 
   const { handleSubmit, register } = useForm({
     defaultValues: {
@@ -22,71 +27,42 @@ function App() {
 
   const onSubmit = (values) => {
     setDate(values.date.toLocaleString())
-    setMission(values.mission)
+    setMission((values.mission === "all") ? URL : roverURL)
   }
+
+
 
   useEffect(() => {
 
-    //WITH FETCH
+    //VERSIÓN CON FETCH
 
-    const getAPOD = async () => {
-      if (mission === "all") {
-
-        const data = await fetch(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`)
-        const nasaToJson = await data.json()
-        return {
-          ...nasaToJson
-        }
-
-      } else {
-        console.log("replace API call below with Rover call")
-        const data = await fetch(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`)
-        const nasaToJson = await data.json()
-        return {
-          ...nasaToJson
-        }
-      }
-
-    }
-
-    getAPOD().then((i) => {
-      setAPOD(i)
-    })
-
-    // ATTEMPT WITH AXIOS
-    //   constaxios = require('axios').default
-    //   const getAPOD = axios.get(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`)
-    //     .then(function (response) {
-    //       // manejar respuesta exitosa
-    //       console.log(response);
-    //     })
+    // const getAPOD = async () => {
+    //     const data = await fetch(mission)
+    //     const nasaToJson = await data.json()
+    //     return {...nasaToJson}
     // }
 
-
-    // const getRoverAPOD = async () => {
-    //   const roverData = await fetch(`${NASA_URL}mars-photos/api/v1/rovers/curiosity/photos?earth-date=2015-3-3&api_key=${NASA_API_KEY}`)
-    //   const roverToJson = await roverData.json()
-    //   return {
-    //     ...roverToJson
-    //   }
-    // }
-
-    // getRoverAPOD().then((j) => {
-    //   setRoverAPOD(j)
-    //   console.log(roverAPOD)
+    // getAPOD().then((i) => {
+    //   setAPOD(i)
     // })
 
+    // VERSIÖN CON AXIOS
 
+    (mission === "rover" ? axios.get(mission) : axios.get(mission))
+      .then((i) => {
+        setAPOD(i.data)
+      })
+      .catch((error) => { console.log(error) })
   }, [date, mission])
 
   return (
     <>
 
       <Header mainTitle={mainTitle} />
-      <p>Esta imagen corresponde con la fecha: <strong>{date}</strong></p>
-      <p> {mission === "rover" ? "Viene de la misión Mars Rover" : null}</p>
+      <p>Esta imagen corresponde con la fecha: <strong>{date}</strong>. {mission === "rover" ? "Viene de la misión Mars Rover" : null}.</p>
       <Form register={register} handleSubmit={handleSubmit} onSubmit={onSubmit} today={today} />
       <Card apod={APOD} />
+      <Footer />
 
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <p>This is not an official NASA website. Made by Penelope with ❤️</p>
+  )
+}
+
+export default Footer
