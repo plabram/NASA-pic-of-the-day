17603b245fd4d9a6ad6a1ee665d6426890025ffa
Penelope <freyalabram@gmail.com>
2023-07-16 17:21:13 +0200

    second set of responsive changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>This is not an official NASA website. Made by Penelope with ❤️</p>
+    </div>
+  )
+}
+
+export default Footer
